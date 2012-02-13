c3d0a3428942a815a70d88dcb60ed06beb832b7f
obfuscurity <jason@dixongroup.net>
2012-02-12 23:51:21 -0500

    more basic validations, add #really_destroy for integration tests

--- models/sites.rb
+++ models/sites.rb
@@ -1,4 +1,10 @@
 
+class Sequel::Model
+  #def validates_json_schema(input)
+  #  errors.add(input, "Invalid JSON Schema") unless Schema.is_valid?(input)
+  #end
+end
+
 class Site < Sequel::Model
 
   many_to_one :users
@@ -9,7 +15,8 @@ class Site < Sequel::Model
   def validate
     super
     validates_presence :name
-    validates_length_range 2..32, :name
+    validates_length_range 2..50, :name
+    #validates_json_schema :schema
   end
 
   def before_create
@@ -42,6 +49,10 @@ class Site < Sequel::Model
   def destroy
     self.enabled = false
     self.save
+  end
+
+  def really_destroy
+    self.delete
   end
 end
 
