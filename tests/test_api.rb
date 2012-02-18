e21f16cfe6338ba7fa07975ef57176d702bd61bc
obfuscurity <jason@dixongroup.net>
2012-02-18 01:51:35 -0500

    test username for rfc822 compliance

--- tests/test_api.rb
+++ tests/test_api.rb
@@ -77,25 +77,31 @@ class VellupApiTest < Test::Unit::TestCase
     assert last_response.status == 400
   end
 
-  def test_08_create_user_invalid_username
-    "Creating a user should fail with an invalid username"
+  def test_08_create_user_short_username
+    "Creating a user should fail with a username too short"
     post "/sites/#{@@site['uuid']}/users/add", {:username => 'x', :password => 'test'}, @@options
     assert last_response.status == 400
   end
 
-  def test_09_create_user_missing_password
+  def test_09_create_user_invalid_username
+    "Creating a user should fail with an invalid username"
+    post "/sites/#{@@site['uuid']}/users/add", {:username => 'xx', :password => 'test'}, @@options
+    assert last_response.status == 400
+  end
+
+  def test_10_create_user_missing_password
     "Creating a user should fail without a password"
     post "/sites/#{@@site['uuid']}/users/add", {:username => 'test_site_user@vellup.com'}, @@options
     assert last_response.status == 400
   end
 
-  def test_10_create_user_invalid_password
+  def test_11_create_user_invalid_password
     "Creating a user should fail with an invalid password"
     post "/sites/#{@@site['uuid']}/users/add", {:username => 'test_site_user@vellup.com', :password => 'x'}, @@options
     assert last_response.status == 400
   end
 
-  def test_11_create_user
+  def test_12_create_user
     "Creating a user for our test site"
     post "/sites/#{@@site['uuid']}/users/add", {:username => 'test_site_user@vellup.com', :password => 'test'}, @@options
     @@site_user = JSON.parse(last_response.body)
@@ -104,7 +110,7 @@ class VellupApiTest < Test::Unit::TestCase
     assert @@site_user['custom'].nil?
   end
 
-  def test_12_list_users
+  def test_13_list_users
     "Listing all users for our test site"
     get "/sites/#{@@site['uuid']}/users", {}, @@options
     assert last_response.status = 200
@@ -117,7 +123,7 @@ class VellupApiTest < Test::Unit::TestCase
   #  assert last_response.status == 400
   #end
 
-  def test_13_modify_user
+  def test_14_modify_user
     "Modifying the custom data blob of our test user"
     put "/sites/#{@@site['uuid']}/users/#{@@site_user['id']}", {:custom => '{"firstname":"Test","lastname":"Tester"}'}, @@options
     @@site_user = JSON.parse(last_response.body)
@@ -126,13 +132,13 @@ class VellupApiTest < Test::Unit::TestCase
     assert_equal JSON.parse(@@site_user['custom'])['lastname'], 'Tester'
   end
 
-  def test_14_get_user_unknown
+  def test_15_get_user_unknown
     "Getting the details of an unknown test user should fail"
     get "/sites/#{@@site['uuid']}/users/00", {}, @@options
     assert last_response.status == 404
   end
 
-  def test_15_get_user
+  def test_16_get_user
     "Getting the details of our test user"
     get "/sites/#{@@site['uuid']}/users/#{@@site_user['id']}", {}, @@options
     @@site_user = JSON.parse(last_response.body)
@@ -141,20 +147,20 @@ class VellupApiTest < Test::Unit::TestCase
     assert_equal JSON.parse(@@site_user['custom'])['lastname'], 'Tester'
   end
 
-  def test_16_auth_user_bad_password
+  def test_17_auth_user_bad_password
     "Authentication should fail for our test user"
     post "/sites/#{@@site['uuid']}/users/auth", {:username => 'test_site_user@vellup.com', :password => 'fail'}, @@options
     assert last_response.status == 401
   end
 
-  def test_17_auth_user
+  def test_18_auth_user
     "Authenticating our test user"
     post "/sites/#{@@site['uuid']}/users/auth", {:username => 'test_site_user@vellup.com', :password => 'test'}, @@options
     assert last_response.status == 200
     assert_equal @@site_user['username'], 'test_site_user@vellup.com'
   end
 
-  def test_18_delete_user
+  def test_19_delete_user
     "Deleting our test user"
     delete "/sites/#{@@site['uuid']}/users/#{@@site_user['id']}", {}, @@options
     assert last_response.status == 204
@@ -162,20 +168,20 @@ class VellupApiTest < Test::Unit::TestCase
     assert user.values[:enabled] == false
   end
 
-  def test_19_get_invalid_user
+  def test_20_get_invalid_user
     "Getting user details should fail after deleting user"
     get "/sites/#{@@site['uuid']}/users/#{@@site_user['id']}", {}, @@options
     assert last_response.status == 404
   end
 
-  def test_20_list_users
+  def test_21_list_users
     "Listing all users for our test site (should return zero)"
     get "/sites/#{@@site['uuid']}/users", {}, @@options
     assert last_response.status = 204
     assert last_response.body.empty?
   end
 
-  def test_21_delete_site
+  def test_22_delete_site
     "Deleting our test site"
     delete "/sites/#{@@site['uuid']}", {}, @@options
     assert last_response.status == 204
@@ -183,19 +189,19 @@ class VellupApiTest < Test::Unit::TestCase
     assert site.values[:enabled] == false
   end
 
-  def test_22_get_invalid_site
+  def test_23_get_invalid_site
     "Getting site details should fail after deleting site"
     get "/sites/#{@@site['uuid']}", {}, @@options
     assert last_response.status == 404
   end
 
-  def test_23_teardown_user
+  def test_24_teardown_user
     "Deleting our test user FOR REAL"
     user = User[@@site_user['id']]
     user.really_destroy
   end
 
-  def test_24_teardown_site
+  def test_25_teardown_site
     "Deleting our test site FOR REAL"
     site = Site.filter(:uuid => @@site['uuid']).first
     site.really_destroy
